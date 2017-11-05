e6ec675e9fea047b8b4689c59506a3abfc7272d3
rehan <acc.rehan@gmail.com>
2017-11-05 19:00:10 +0400

    My secong commit with host change

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rfxj#(s=#vhrx#-#&m3v&*sur&ivcf_5wj&sy-7nf_bgi85b&&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'reqam.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
